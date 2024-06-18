f67d5b73fa1a96f11f730481654a7e6de4e070ec
dudrldudwls12 <cacasi376@gmail.com>
2024-06-18 19:46:17 +0900

    (2024.6.18) 벡터

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -1,79 +1,30 @@
 ﻿#include <iostream>
 
 using namespace std;
-
-#define SIZE 10
-template<typename T>
-class Stack
+#define SIZE 5
+template <typename T>
+class CircleQueue
 {
 private:
-	int top;
-	T container[SIZE];
+    int size;
+    int rear;
+    int front;
+    T  container[SIZE];
 public:
-	Stack()
-	{
-		top = -1;
-		for (int i = 0; i < SIZE; i++)
-		{
-			container[i] = 0;
-		}
-	}
-	void Push(T data)
-	{
-		if (top >= SIZE-1)
-		{
-			cout << "Stack Overflow" << endl;
-		}
-		else 
-		{
-			container[++top] = data;
-		}
-	}
-	void Pop()
-	{
-		if (Empty)
-		{
-			cout << "Stack is Empty" << endl;
-		}
-		else
-		{
-			top--;
-		}
-	}
-	bool Empty()
-	{
-		if (top <= -1)
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	int & Size()
-	{
-		return top;
-	}
-	T & Top()
-	{
-		return container[top];
-	}	
+    CircleQueue()
+    {
+        size = 0;
+        rear = SIZE - 1;
+        front = SIZE - 1;
+        for (int i = 0; i < SIZE; i++)
+        {
+            container[i] = SIZE;
+        }
+    }
 };
-bool CheckBracket(std::string content)
-	{
-	Stack<char> bracket;
-		{
-		if (content.length() <= 0)
-		{
-			{
-				return false;
-			}
-		}
-	}	
+
 int main()
 {
-	Stack<int> stack;
 
-	return 0;
+    return 0;
 }
