3627999b82133fb4d0e9e35a32241f1d63d0424c
dudrldudwls12 <cacasi376@gmail.com>
2024-06-24 19:47:30 +0900

    (2024.6.24) 이진 트리

--- Program/Program.cpp
+++ Program/Program.cpp
@@ -10,13 +10,15 @@ private:
     {
         KEY key;
         VALUE value;
-        Node * next;
+
+        Node* next;
     };
     struct Bucket
     {
         int count;
         Node* head;
     };
+    Bucket bucket[SIZE];
 public:
     HashTable()
     {
@@ -26,9 +28,57 @@ public:
             bucket[i].head = nullptr;
         }
     }
+    template <typename T>
+    int HashFunction(T key)
+    {
+        int hashIndex = (int)key % SIZE;
+
+        return hashIndex;
+    }
+    template<>
+    int HashFunction(std::string key)
+    {
+        int result = 0;
+        for (const char& element : key)
+        {
+            result += element;
+        }
+        int hashIndex = (int)result % SIZE;
+        return hashIndex;
+    }
+    void Insert(KEY key, VALUE value)
+    {
+        // 1. 해시 함수를 통해서 값을 받는 임시 변수
+        int hashIndex = HashFunction(key);
+        // 새로운 노드를 생성합니다.
+        Node * newNode = CreateNode(key, value);
+        // 노드가 1개라도 존재하지 않는다면
+        if (bucket[hashIndex].count == 0)
+        {
+            // bucket[hashIndex]의 head 포인터를 newNode를 가리키게 합니다.
+            bucket[hashIndex].head = newNode;
+        }
+        else
+        {
+            newNode->next = bucket[hashIndex].head;
+            bucket[hashIndex].head = newNode;
+        }
+        // bucket[hashIndex]의 count를 증가합니다.
+        bucket[hashIndex].count++;
+    }
+    Node * CreateNode(KEY key, VALUE value)
+    {
+        Node * newNode = new Node();
+        newNode->key = key;
+        newNode->value = value;
+        newNode->next = nullptr;
+        return newNode;
+    }
+
 };
 int main()
 {
- 
+    HashTable<std::string, std::string>    hashTable;
+    hashTable.Insert("Sword")
     return 0;
 }
